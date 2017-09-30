e581697b9992e5770659c8adc50c62a01e2a98e9
Boy <hackersky109@gmail.com>
2017-09-30 23:09:18 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,12 +3,28 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    lines = tsv.split("\n")
+    keys = lines[0].split("\t")
+    result = []
+    lines[1..lines.length].each do |line|
+      structure = {}
+      line.split("\t").each_with_index { |item, index| structure[keys[index]] = item}
+      result << structure
+    end
+    @data = result
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    keys = []
+    @data[0].each_key { |key| keys << key }
+    result = keys.join("\t") + "\n"
+    @data.each do |obj|
+      line = []
+      obj.each_key { |key| line << obj[key] }
+      result << line.join("\t") + "\n"
+    end
+    result
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,17 @@
 # Module that can be included (mixin) to take and output Yaml data
+require 'yaml'
+# Module that can be included (mixin) to take and output TSV data
+module YmlBuddy
+  # take_tsv: converts a String with TSV data into @data
+  # parameter: tsv - a String in TSV format
+  def take_yaml(yaml)
+    @data = YAML.load(yaml)
+  end
+
+  # to_tsv: converts @data into tsv string
+  # returns: String in TSV format
+  def to_yaml
+    @data = @data.to_yaml
+    @data
+  end
+end
